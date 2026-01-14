89a85e7bd9c9c3c408bd4b8b0b69c5f360145e9c
Jarlok17 <abilik48@gmail.com>
2026-01-14 15:23:26 +0200

    feat: add cli entry point

---
+++ src/cli/app.cpp
@@ -0,0 +1,50 @@
+#include "app.hpp"
+#include <cstdlib>
+#include <exception>
+#include <iostream>
+
+#include "commands/include/file.hpp"
+#include "commands/include/help.hpp"
+#include "commands/include/pipe.hpp"
+#include "commands/include/run.hpp"
+#include "vanitas/args_parser.hpp"
+
+namespace vanitas::cli {
+int CliApp::run(int argc, char *const argv[])
+{
+    try {
+        vanitas::ArgsParser parser(argc, argv);
+        vanitas::Args args = parser.parse();
+
+        int rc = 0;
+
+        switch (args.mode) {
+        case vanitas::Mode::Help: {
+            HelpCommand cmd(args);
+            rc = cmd.execute();
+            break;
+        }
+        case vanitas::Mode::File: {
+            FileCommand cmd(args);
+            rc = cmd.execute();
+            break;
+        }
+        case vanitas::Mode::Pipe: {
+            PipeCommand cmd(args);
+            rc = cmd.execute();
+            break;
+        }
+        case vanitas::Mode::Run: {
+            RunCommand cmd(args);
+            rc = cmd.execute();
+            break;
+        }
+        }
+
+        std::exit(rc);
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << "\n";
+        std::exit(2);
+    }
+}
+} // namespace vanitas::cli
---
+++ src/cli/app.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace vanitas::cli {
+struct CliApp
+{
+        int run(int argc, char *const argv[]);
+};
+} // namespace vanitas::cli
